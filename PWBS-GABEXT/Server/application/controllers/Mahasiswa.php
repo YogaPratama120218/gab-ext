ec1b38ee0d0d3796dcc474baba66f1b3f064e3e2
YogaPratama120218 <muhammad_rizki_yoga_pratama@teknokrat.ac.id>
2022-10-21 18:33:20 +0700

    perubahan isi status post

--- PWBS-GABEXT/Server/application/controllers/Mahasiswa.php
+++ PWBS-GABEXT/Server/application/controllers/Mahasiswa.php
@@ -63,7 +63,7 @@ Class Mahasiswa extends Server {
             if ($hasil == 0)
             {
                     //tampilkan status dalam format "JSON"
-                    $this->response(array("status" => "Data Gagal Disimpan!"),200);
+                    $this->response(array("status" => "Data berhasil disimpan ..."),200);
            
             }
             //jika data mahasiswa ditemukan
